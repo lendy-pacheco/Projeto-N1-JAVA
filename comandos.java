92e9223f40b43bf1d4263b8eee0c7680230bd23e
Victor Reis <reisdasilva.victor@outlook.com>
2025-03-31 16:37:16 -0300

    Create comandos.java

---
+++ comandos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class comandos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Criando o objeto Scanner
+        Fila comando = new Fila(10);
+
+        char grav;
+        while(!comando.qIsFull()){
+
+            if(comando == "EXIT"){
+                break;
+            }
+            else{
+
+            }
+
+        }
+        
+    }
+    
+}
