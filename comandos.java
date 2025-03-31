814766500f3be53dd1f7064016d753e704891bda
Victor Reis <reisdasilva.victor@outlook.com>
2025-03-31 18:31:36 -0300

    Update comandos.java

--- comandos.java
+++ comandos.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
-public class comandos {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Criando o objeto Scanner
-        Fila comando = new Fila(10);
-
-        char grav;
-        while(!comando.qIsFull()){
-
-            if(comando == "EXIT"){
-                break;
-            }
-            else{
-
-            }
+public class comando {
+    public void comandos(String grav){
+        if(grav.equals("VARS")){
 
         }
-        
+        else if (grav.equals("REC")){
+            Rec();
+
+        }
+        else if(grav.equals("PLAY")){
+
+        }
+        else if (grav.equals("ERASE")){
+
+        }
+        else if (grav.equals("EXIT"));
+
     }
-    
 }
