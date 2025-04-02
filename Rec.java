b5219b606044767b8555abcc49a06384a0da2874
Victor Reis <reisdasilva.victor@outlook.com>
2025-04-02 18:11:32 -0300

    Update Rec.java

--- Rec.java
+++ Rec.java
@@ -1,35 +1,29 @@
 import java.util.Scanner;
+
 public class Rec {
-    public  Fila loop_de_gravação(Fila gravacao) {
+    public Fila loop_de_gravação(Fila gravacao) {
         System.out.println("iniciando gravação de até 10 elementos ... \n");
         Scanner scanner = new Scanner(System.in); // Criando o objeto Scanner
-        int count=1; //contador para gravação
+        int count = 1; // Contador para gravação
 
-        while(!gravacao.qIsFull()){ // empilha as entradadas
+        while (!gravacao.qIsFull()) { // Empilha as entradas
             System.out.printf("gravando (%d/10)", count);
             System.out.print("< ");
-            String grav= scanner.nextLine().toUpperCase(); // imput do usuario para gravar
+            String grav = scanner.nextLine().toUpperCase(); // Input do usuário para gravar
 
             if (grav.equals("PLAY") || grav.equals("REC") || grav.equals("ERASE") || grav.equals("EXIT")) {
                 System.out.println("Erro: comando inválido para gravação.");
             }
-
-            else if(grav.equals("STOP")){ //parar gravação
+            else if (grav.equals("STOP")) { // Parar gravação
                 break;
             }
-            else{
+            else {
                 count++;
-                gravacao.enqueue(grav); //coloca os comandos na fila gravação
-               // transformar.vetor(grav);
+                gravacao.enqueue(grav); // Coloca os comandos na fila de gravação
             }
-
         }
-        scanner.close();
-        REPL repl = new REPL(); // cria obj repl
-        repl.iniciarREPL();// volta ao menu REPL
 
-        return(gravacao);
-        
+        //  NÃO FECHAR O SCANNER AQUI! O REPL ainda precisa usá-lo.
+        return gravacao; // Retorna a fila gravada 
     }
-    
 }
